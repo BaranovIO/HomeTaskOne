8902b890b8166435bbb4a293e189ee3f7a7538a7
Igor <eltery@mail.ru>
2022-11-30 12:33:55 +0300

    max number between two numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input another number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("max = " + num1);
+}
+else
+{
+    Console.Write("max = " + num2);
+}
